4fc87c1883fdfbe862ccd4b2fafff33ce423a3cf
Col Harris <col.w.harris@gmail.com>
2018-03-18 23:41:15 +0800

    Fix the models package references

--- telegram/main.go
+++ telegram/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"whosinbot/telegram/models"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/col/whosinbot/models"
 )
 
-func Handler() (APIGatewayResponse, error) {
-	response := Response{
+func Handler() (models.APIGatewayResponse, error) {
+	response := models.Response{
 		Message: "WhosInBot",
 	}
 
