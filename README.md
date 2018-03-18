a1c1dded427628886ef2cc833aa4bf8c68b1e967
Col Harris <col.w.harris@gmail.com>
2018-03-19 01:27:44 +0800

    Add telegram bot framework and implement simple echo bot

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ## Register webhook
 
+Set Webhook
 ```
-curl -XPOST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/setWebhook?url=$WEBHOOK_URL
+curl -XPOST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/setWebhook?url=$WEBHOOK_URL/$TELEGRAM_BOT_TOKEN
+```
+
+Get Webhook Info
+```
+curl -XPOST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/getWebhookInfo
 ```
--- telegram/main.go
+++ telegram/main.go
@@ -1,21 +1,63 @@
 package main
 
 import (
+	"os"
+	"fmt"
+	"errors"
 	"context"
-	"whosinbot/telegram/models"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"gopkg.in/telegram-bot-api.v4"
+	"encoding/json"
 )
 
+func ValidateToken(requestToken string) (string, error) {
+	token := os.Getenv("TELEGRAM_BOT_TOKEN")
+	if token != requestToken {
+		message := fmt.Sprintf("Bot token doesn't match! Expected: %v Received: %v", token, requestToken)
+		return "", errors.New(message)
+	}
+	return token, nil
+}
+
+func ParseRequest(requestBody string) (*tgbotapi.Update, error) {
+	update := &tgbotapi.Update{}
+	err := json.Unmarshal([]byte(requestBody), update)
+	return update, err
+}
+
+func HandleUpdate(update *tgbotapi.Update, bot *tgbotapi.BotAPI) {
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+	bot.Send(msg)
+}
+
+func response(err error) (events.APIGatewayProxyResponse, error) {
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 400}, err
+	} else {
+		return events.APIGatewayProxyResponse{StatusCode: 200}, nil
+	}
+}
+
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	response := models.Response{
-		Message: "WhosInBot - " + request.PathParameters["token"],
+	token, err := ValidateToken(request.PathParameters["token"])
+	if err != nil {
+		return response(err)
 	}
 
-	return events.APIGatewayProxyResponse{
-		Body: response.String(),
-		StatusCode: 200,
-	}, nil
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		return response(err)
+	}
+
+	update, err := ParseRequest(request.Body)
+	if err != nil {
+		return response(err)
+	}
+
+	HandleUpdate(update, bot)
+
+	return response(nil)
 }
 
 func main() {
