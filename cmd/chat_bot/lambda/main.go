8a8bdcb7e45f96addd1796c7cba5e98990be2ec1
Gemeng Qin <gqin@thoughtworks.com>
2018-10-10 19:24:47 +0800

    Support hangouts chat bot

---
+++ cmd/chat_bot/lambda/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"google.golang.org/api/chat/v1"
+	"log"
+	"whosinbot/chat_bot"
+	"whosinbot/dynamodb"
+	"whosinbot/whosinbot"
+)
+
+func main() {
+	lambda.Start(Handler)
+}
+
+func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Printf("Request Body: " + request.Body)
+
+	command, err := chat_bot.ParseDeprecatedEvent([]byte(request.Body))
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 400}, err
+	}
+
+	// Process Command
+	dataStore := dynamodb.NewDynamoDataStore()
+	bot := whosinbot.WhosInBot{DataStore: dataStore}
+	response, err := bot.HandleCommand(command)
+	if err != nil {
+		return events.APIGatewayProxyResponse{StatusCode: 400}, err
+	}
+
+	// Send Response
+	if response == nil {
+		return events.APIGatewayProxyResponse{StatusCode: 400}, nil
+	}
+
+	message := chat.Message{
+		Text: response.Text,
+	}
+	messageJOSN, _ := message.MarshalJSON()
+
+	resBody := string(messageJOSN)
+	log.Printf("response body: %s\n", resBody)
+	return events.APIGatewayProxyResponse{StatusCode: 200, Body: resBody}, nil
+}
+
+
