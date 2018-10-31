9692d586728c03864007236a2581a17cbfbdce41
Gemeng Qin <gqin@thoughtworks.com>
2018-10-31 23:24:44 +0800

    Allow to use alias

--- hangout/helpers.go
+++ hangout/helpers.go
@@ -9,6 +9,27 @@ import (
 	"errors"
 )
 
+var (
+	allCommands []string = []string{
+		"start_roll_call",
+		"end_roll_call",
+		"set_title",
+		"in",
+		"out",
+		"maybe",
+		"set_in_for",
+		"set_out_for",
+		"set_maybe_for",
+		"whos_in",
+		"shh",
+		"louder",
+	}
+	startRollCallAlias string = "start"
+	endRollCallAlias   string = "end"
+	whosInAlias        string = "ls"
+	setTitleAlias      string = "title"
+)
+
 func ParseDeprecatedEvent(requestBody []byte) (domain.Command, error) {
 	deprecatedEvent := chat.DeprecatedEvent{}
 	err := json.Unmarshal(requestBody, &deprecatedEvent)
@@ -17,16 +38,22 @@ func ParseDeprecatedEvent(requestBody []byte) (domain.Command, error) {
 	}
 
 	log.Printf("deprecatedEvent: " + deprecatedEvent.Message.Text)
-	
+
 	// TODO: split this into a mapping function and write some tests around it
 	threadName := deprecatedEvent.Message.Thread.Name
 	arguments := strings.Fields(deprecatedEvent.Message.ArgumentText)
 	if len(arguments) <= 0 {
 		return domain.EmptyCommand(), errors.New("no argument provided")
 	}
+	
+	name, err := parseCommandName(arguments[0])
+	if err != nil {
+		return domain.EmptyCommand(), err
+	}
+
 	command := domain.Command{
 		ChatID: threadName,
-		Name: arguments[0],
+		Name:   name,
 		Params: arguments[1:],
 		From: domain.User{
 			UserID: deprecatedEvent.Message.Sender.Name,
@@ -34,4 +61,38 @@ func ParseDeprecatedEvent(requestBody []byte) (domain.Command, error) {
 		},
 	}
 	return command, err
+}
+
+func contains(set []string, element string) bool {
+	for _, oneEle := range set {
+		if oneEle == element {
+			return true
+		}
+	}
+	return false
+}
+
+func parseCommandName(commandNameArgument string) (string, error) {
+	if contains(allCommands, commandNameArgument){
+		return commandNameArgument, nil
+	}
+
+	alias := strings.ToLower(commandNameArgument)
+	if alias == startRollCallAlias {
+		return "start_roll_call", nil
+	}
+
+	if alias == endRollCallAlias {
+		return "end_roll_call", nil
+	}
+
+	if alias == whosInAlias {
+		return "whos_in", nil
+	}
+
+	if alias == setTitleAlias {
+		return "set_title", nil
+	}
+
+	return "", errors.New("invalid")
 }
--- hangout/helpers_test.go
+++ hangout/helpers_test.go
@@ -10,6 +10,89 @@ import (
 )
 
 var _ = Describe("helper", func() {
+	Describe("#parseCommandName", func() {
+		When("command name is in the commands set", func() {
+			It("returns the original name", func() {
+				commandNmae, err := parseCommandName("start_roll_call")
+				Expect(commandNmae).To(Equal("start_roll_call"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("end_roll_call")
+				Expect(commandNmae).To(Equal("end_roll_call"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("set_title")
+				Expect(commandNmae).To(Equal("set_title"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("in")
+				Expect(commandNmae).To(Equal("in"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("out")
+				Expect(commandNmae).To(Equal("out"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("maybe")
+				Expect(commandNmae).To(Equal("maybe"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("set_in_for")
+				Expect(commandNmae).To(Equal("set_in_for"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("set_out_for")
+				Expect(commandNmae).To(Equal("set_out_for"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("set_maybe_for")
+				Expect(commandNmae).To(Equal("set_maybe_for"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("whos_in")
+				Expect(commandNmae).To(Equal("whos_in"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("shh")
+				Expect(commandNmae).To(Equal("shh"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("louder")
+				Expect(commandNmae).To(Equal("louder"))
+				Expect(err).NotTo(HaveOccurred())
+
+			})
+		})
+
+		When("command argument is an alias of a command", func() {
+			It("returns original command name", func() {
+				commandNmae, err := parseCommandName("start")
+				Expect(commandNmae).To(Equal("start_roll_call"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("end")
+				Expect(commandNmae).To(Equal("end_roll_call"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("ls")
+				Expect(commandNmae).To(Equal("whos_in"))
+				Expect(err).NotTo(HaveOccurred())
+
+				commandNmae, err = parseCommandName("title")
+				Expect(commandNmae).To(Equal("set_title"))
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+
+		When("command argument not found", func() {
+			It("returns \"\" and error", func() {
+				commandNmae, err := parseCommandName("blah")
+				Expect(commandNmae).To(Equal(""))
+				Expect(err).To(BeEquivalentTo(errors.New("invalid")))
+			})
+		})
+	})
+
 	Describe("#ParseDeprecatedEvent", func() {
 		When("successful", func() {
 			It("parses request body to DeprecatedEvent", func() {
@@ -18,7 +101,7 @@ var _ = Describe("helper", func() {
 					ConfigCompleteRedirectUrl: "",
 					EventTime:                 "",
 					Message: &chat.Message{
-						ArgumentText: "   cmdName argument1   argument2",
+						ArgumentText: "   in argument1   argument2",
 						Sender: &chat.User{
 							DisplayName: "Ryuichi Sakamoto",
 							Name:        "users/1234567",
@@ -32,17 +115,30 @@ var _ = Describe("helper", func() {
 				command, _ := ParseDeprecatedEvent(requestBody)
 
 				Expect(command.ChatID).To(Equal("thread1"))
-				Expect(command.Name).To(Equal("cmdName"))
+				Expect(command.Name).To(Equal("in"))
 				Expect(command.Params).To(Equal([]string{"argument1", "argument2"}))
 				Expect(command.From).To(BeEquivalentTo(domain.User{
 					UserID: "users/1234567",
 					Name:   "Ryuichi Sakamoto",
 				}))
 			})
+
+			Context("parses alias to valid cammand name", func() {
+				It("replaces 'start' as start_roll_call command", func() {
+					deprecatedEvent := mockEvent()
+					deprecatedEvent.Message.ArgumentText = "start foo bar"
+
+					requestBody, _ := json.Marshal(deprecatedEvent)
+					command, _ := ParseDeprecatedEvent(requestBody)
+
+					Expect(command.Name).To(Equal("start_roll_call"))
+					Expect(command.Params).To(Equal([]string{"foo", "bar"}))
+				})
+			})
 		})
 
 		When("failed", func() {
-			Context("when there is no arguments provided", func() {
+			When("there is no arguments provided", func() {
 				deprecatedEvent := getEventWithNoArguments()
 
 				It("returns empty command", func() {
@@ -56,11 +152,11 @@ var _ = Describe("helper", func() {
 					requestBody, _ := json.Marshal(deprecatedEvent)
 					_, err := ParseDeprecatedEvent(requestBody)
 
-					Expect(err).To(BeEquivalentTo( errors.New("no argument provided")))
+					Expect(err).To(BeEquivalentTo(errors.New("no argument provided")))
 				})
 			})
 
-			Context("when can't be unmarshaled to event", func() {
+			When("can't be unmarshaled to event", func() {
 				requestBody, _ := json.Marshal([]byte("something"))
 
 				It("returns empty command", func() {
@@ -75,9 +171,42 @@ var _ = Describe("helper", func() {
 					Expect(err).To(HaveOccurred())
 				})
 			})
+
+			When("fails to get the command name", func() {
+				deprecatedEvent := mockEvent()
+				deprecatedEvent.Message.ArgumentText = "blah"
+
+				It("return empty command and error", func() {
+					requestBody, _ := json.Marshal(deprecatedEvent)
+					command, err := ParseDeprecatedEvent(requestBody)
+
+					Expect(command).To(BeEquivalentTo(domain.EmptyCommand()))
+					Expect(err).To(HaveOccurred())
+				})
+
+			})
+
 		})
 	})
 })
+
+func mockEvent() chat.DeprecatedEvent {
+	return chat.DeprecatedEvent{
+		Action:                    nil,
+		ConfigCompleteRedirectUrl: "",
+		EventTime:                 "",
+		Message: &chat.Message{
+			ArgumentText: "   cmdName argument1   argument2",
+			Sender: &chat.User{
+				DisplayName: "Ryuichi Sakamoto",
+				Name:        "users/1234567",
+			},
+			Thread: &chat.Thread{
+				Name: "thread1",
+			},
+		},
+	}
+}
 
 func getEventWithNoArguments() chat.DeprecatedEvent {
 	return chat.DeprecatedEvent{
